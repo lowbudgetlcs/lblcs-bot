aea9c37ce6d99ff9baa8b008ca5a3173c32fd7a1
ruuffian <ruuffia@gmail.com>
2024-02-02 17:20:23 -0500

    Changes to tournament cog, needs to be merged with dev branch

--- discord_server_scraper/__main__.py
+++ discord_server_scraper/__main__.py
@@ -1,22 +1,33 @@
 import discord
+from discord.ext import commands
 import os
 import user
-import csv
 from dotenv import load_dotenv
+import logging
 
-load_dotenv('.env')
+load_dotenv('../.env')
 TOKEN = os.getenv('TOKEN')
+GUILD_ID = int(os.getenv('GUILD_ID'))
+class Client(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(command_prefix='', intents=intents)
 
-intents = discord.Intents.all()
+    async def on_ready(self):
+        print(f'Logged in as {self.user} ID {self.application_id}')
 
-client = discord.Client(intents=intents)
+        users = []
+        members = self.get_guild(GUILD_ID).members
+        for member in members:
+            users.append(user.User(member.id, member.name, member.display_name))
+        print(users)
 
-@client.event
-async def on_ready():
-    users = []
-    members = client.guilds[0].members
-    for member in members:
-        users.append(user.User(member.id,member.name,member.display_name))
-        
+    async def setup_hook(self):
+        await self.load_extension('tournaments')
+        await self.tree.sync(guild=self.get_guild(GUILD_ID))
 
-client.run(TOKEN)
+
+client = Client()
+
+client.run(TOKEN, log_level=logging.DEBUG)
